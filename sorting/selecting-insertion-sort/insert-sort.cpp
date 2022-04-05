ac584d8fc0555b3259096d512b5cfb7bcc8b2c47
lyuhuafeng <lyuhuafeng@sina.com.cn>
2022-04-05 10:53:27 +0800

    selection sort, insertion sort

---
+++ sorting/selecting-insertion-sort/insert-sort.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <utility> // swap()
+#include <iostream>
+using namespace std;
+
+void insert_sort(int a[], int n) {
+    for (int i = 1; i <= n - 1; i++) {
+        // 把位置 i 的元素(未排序区第一个元素)放到到合适的位置
+        // 如果 a[i] 比前一个小，说明 a[i] 位置不对，需要挪。
+        if (a[i] < a[i - 1]) {
+            int t = a[i];   // t: 未排序区的第一个元素，记录下来，待后面使用
+            int j;
+            for (j = i - 1; j >= 0 && a[j] > t; j--) {  // a[j]>t 而不是>=t，保持稳定性
+                a[j + 1] = a[j];    // 位置j的元素右移，位置j可认为已经空了(尽管实际上还在)
+            }
+            // j循环结束后，所有比t大的元素都右移了一位，给t腾出了地方。
+            // 把t放到空出来的地方 (j+1) 即可。+1是因为循环结束时多减了1.
+            a[j + 1] = t;
+        }
+    }
+}
+
+void insert_sort_2(int a[], int n) {
+    for (int i = 1; i <= n - 1; i++) {
+        // 把 a[i] (未排序区第一个元素) 插入到合适的位置
+        // 只要后面的小（已排序区是从小到大排），就往前交换
+        for (int j = i - 1; j >= 0 && a[j + 1] < a[j] ; j--) {
+            swap(a[j + 1], a[j]);
+        }
+    }
+}
+
+void display(int nums[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int nums[] = {6, 1, 7, 8, 9, 3, 5, 4, 2};
+    insert_sort(nums, sizeof nums / sizeof(int));
+    display(nums, sizeof nums / sizeof nums[0]);
+}
---
+++ sorting/selecting-insertion-sort/select-sort.cpp
@@ -0,0 +1,40 @@
+#include <utility>  // swap()
+using namespace std;
+
+void display(int[], int);
+
+void select_sort(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        // 从未排序区 [i, n - 1] 里挑出最小的
+        // 位置 i 是未排序区的起点。
+        // [0, i - 1] 是已排序区。
+        int min_num = a[i];
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < min_num) {
+                min_num = a[j];
+                min_index = j;
+            }
+        }
+        // 未排序区里最小的 (min_index) 与未排序区第一个 (i) 交换
+        // 位置 i 变成已排序区的最后一个
+        swap(a[min_index], a[i]);
+        display(a, n);
+    }
+}
+
+int main() {
+    int nums[] = {6, 1, 9, 7, 8, 3, 5, 4, 2, 0};
+    int len = sizeof nums / sizeof(int);
+    display(nums, len);
+    select_sort(nums, len);
+    display(nums, len);
+    return 0;
+}
+
+void display(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
