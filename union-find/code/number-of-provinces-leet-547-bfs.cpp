ac60af23041871633d402763a49f81bca02c5687
Lyu Huafeng <lvhuafeng@gmail.com>
2023-12-08 15:21:20 +0800

    union-find set

---
+++ union-find/code/number-of-provinces-leet-547-bfs.cpp
@@ -0,0 +1,59 @@
+// 改进方法 1
+// 每个连通分量用一个 set 存储。多个 set 放在一个 vector 里。
+// visited[i] 表示 i 是否已访问过，true/false，用在 bfs 中。
+// 每个节点都 bfs 完成后，连通分量的数量为 v.size()
+int findCircleNum(vector<vector<int>>& isConnected) {
+    int n = isConnected.size();
+    vector<bool> visited(n, false); // 都没访问过
+    vector<unordered_set<int>> v;
+    for (int i = 0; i < n; i++) {
+        if (visited[i]) {
+            continue;
+        }
+        unordered_set<int> s;
+        queue<int> q;
+        q.push(i);
+        while (!q.empty()) {
+            int k = q.front();
+            q.pop();
+            for (int j = 0; j < n; j++) {
+                if (isConnected[k][j] > 0 and !visited[j]) {
+                    s.insert(j);
+                    visited[j] = true;
+                    q.push(j);
+                }
+            }
+        }
+        v.push_back(s);
+    }
+    return v.size();
+}
+
+// 改进方法 2
+// 不用 set 及其 vector 记录各连通分量了。
+// bool visited[i] 改为 int prov[i]，表示 i 属于哪个连通分量。若 i=0 表示还没访问过。用在 bfs 中。
+// 用 cnt 记录发现了几个连通分量，省去了 set、vector<set> 以及最后 v.size()
+int findCircleNum(vector<vector<int>>& isConnected) {
+    int n = isConnected.size();
+    vector<int> prov(n, 0); // prov[i]: i属于哪个省
+    int cnt = 0; // 从1开始
+    for (int i = 0; i < n; i++) {
+        if (prov[i] > 0) {
+            continue;
+        }
+        cnt++;
+        queue<int> q;
+        q.push(i);
+        while (!q.empty()) {
+            int k = q.front();
+            q.pop();
+            for (int j = 0; j < n; j++) {
+                if (isConnected[k][j] > 0 and prov[j] == 0) {
+                    prov[j] = cnt;
+                    q.push(j);
+                }
+            }
+        }
+    }
+    return cnt;
+}
---
+++ union-find/code/number-of-provinces-leet-547-union-find.cpp
@@ -0,0 +1,59 @@
+class Solution {
+
+private:
+    class union_find {
+    private:
+        vector<int> parents; // 链表, 若 parents[i] == i 则 i 为树的 root
+        vector<int> rank; // rank[i]: i所在树的 rank
+        int merged_count = 0;
+    public:
+        int disjoint_count = 0;
+
+    public:
+        union_find(const vector<vector<int>> & isConnected) {
+            int n = isConnected.size();
+            for (int i = 0; i < n; i++) {
+                parents.push_back(i);
+                rank.push_back(1);
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (isConnected[i][j] == 1) {
+                        unite(i, j);
+                    }
+                }
+            }
+            disjoint_count = n - merged_count;
+        }
+        // 找 i 所在的树的 root，并顺便压缩路径
+        int find(int i) {
+            if (parents[i] == i) {
+                return i;
+            }
+            parents[i] = find(parents[i]);
+            return parents[i];
+        }
+        // 合并 i、j 所在的树。rank 小的，挂到另外一棵树的 root 上。
+        void unite(int i, int j) {
+            int ir = find(i), jr = find(j); // 二者所在树的 root
+            if (ir != jr) { // 两个 root 不同，是不同的树，才需要合并
+                merged_count++;
+                // rank 小的，挂到另外一棵树的 root 上
+                if (rank[ir] <= rank[jr]) {
+                    parents[ir] = jr;
+                } else {
+                    parents[jr] = ir;
+                }
+                if (rank[ir] == rank[jr]) {
+                    rank[ir]++;
+                }
+            }
+        }
+    };
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        union_find uf(isConnected);
+        return uf.disjoint_count;
+    }
+};
