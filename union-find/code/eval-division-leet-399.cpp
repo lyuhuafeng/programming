b6695ed6226b5602f4e73768a8e56ae3cc4ab455
Lyu Huafeng <lvhuafeng@gmail.com>
2023-12-08 22:15:20 +0800

    union find, leet 399

---
+++ union-find/code/eval-division-leet-399.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+void display(unordered_map<string, double>& val, const unordered_map<string, string>& parents) {
+    for (auto e : parents) {
+        printf(" %s -> %s, %f\n", e.first.c_str(), e.second.c_str(), val[e.first]);
+    }
+}
+
+vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+    unordered_map<string, double> val;
+    unordered_map<string, string> parents;
+
+    for (int i = 0; i < equations.size(); i++) {
+        string u = equations[i][0];
+        string v = equations[i][1];
+        if (parents.count(u) == 0 && parents.count(v) == 0) { // u, v 均未处理过：以 v 为 root
+            parents[v] = v;
+            parents[u] = v;
+            val[v] = 1.0;
+            val[u] = values[i];
+        } else if (parents.count(u) == 0) { // 只有 u (分子) 未处理过
+            parents[u] = parents[v];
+            val[u] = val[v] * values[i];
+        } else if (parents.count(v) == 0) { // 只有 v (分母) 未处理过
+            parents[v] = parents[u];
+            val[v] = val[u] / values[i];
+        } else {
+            string ur = parents[u], vr = parents[v];
+            if (ur != vr) { // u,v 均处理过，且在不同的树
+                double factor = val[u] / values[i] / val[v];
+                // 遍历 v 所在的树，都直接挂到 ur 上
+                for (auto e : parents) {
+                    if (e.second == vr) {
+                        val[e.first] *= factor;
+                        parents[e.first] = ur;
+                    }
+                }
+            } else { // ur == vr: u, v 均处理过，且在同一棵树：不应该
+                // does nothing.
+            }
+        }
+        // display(val, parents);
+    }
+
+    vector<double> ans;
+    for (vector<string> q : queries) {
+        string u = q[0], v = q[1];
+        if (val.count(u) == 0 || val.count(v) == 0) {
+            ans.push_back(-1.0);
+        } else {
+            string ur = parents[u], vr = parents[v];
+            if (ur != vr) {
+                ans.push_back(-1.0);
+            } else {
+                ans.push_back(val[u] / val[v]);
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<vector<string>> equations = {{"a","b"},{"e","f"},{"b","e"}};
+    vector<double> values = {3.4,1.4,2.3};
+    vector<vector<string>> queries = {{"b","a"},{"a","f"},{"f","f"},{"e","e"},{"c","c"},{"a","c"},{"f","e"}};
+    vector<double> ans = calcEquation(equations, values, queries);
+    for (double val : ans) {
+        printf("%f, ", val);
+    }
+    printf("\n");
+    return 0;
+}
--- union-find/problem-list.md
+++ union-find/problem-list.md
@@ -4,8 +4,8 @@
 
 标准模版，不多说。
 
-- 并查集方法 [number-of-provinces-leet-547-union-find.cpp](code/number-of-provinces-leet-547-union-find.cpp)
-- bfs 方法 [number-of-provinces-leet-547-bfs.cpp](code/number-of-provinces-leet-547-bfs.cpp)
+- 标准并查集方法 [number-of-provinces-leet-547-union-find.cpp](code/number-of-provinces-leet-547-union-find.cpp)
+- 我的 bfs 方法 [number-of-provinces-leet-547-bfs.cpp](code/number-of-provinces-leet-547-bfs.cpp)
 
 ## [leetcode 684. 冗余连接](https://leetcode.cn/problems/redundant-connection/)
 
@@ -13,5 +13,15 @@
 
 每次输入一条 edge。parent[i] 先初始化为 0（无 root）而不是 i（自己是 root）。每次来一条 edge，直接处理两个顶点，parent。find()、union() 都有所改动。
 
-- 并查集方法 [redundant-edge-leet-684.cpp](code/redundant-edge-leet-684.cpp)
+- 我的并查集方法 [redundant-edge-leet-684.cpp](code/redundant-edge-leet-684.cpp)
+- [多种方法 by yukiyama](https://leetcode.cn/problems/redundant-connection/solutions/1594870/-by-yukiyama-mlqi/)
+
+## [leetcode 399. 除法求值](https://leetcode.cn/problems/evaluate-division/submissions/)
+
+与 `leetcode 684. 冗余连接` 有点像，都是逐条 edge 输入。
+
+写代码写到 union 两棵不同的树时，发现，被合并的树上，每个顶点的 val 都要更新，完全可以每个顶点都直接挂到 ur 上去。这样，始终不会形成链表，`find(i)` 可以直接用 `parents[i]` 得到，也不需要 `rank[]` 数组了。
+
+- 我的并查集方法 [eval-division-leet-399.cpp](code/eval-division-leet-399.cpp)
+- [yukiyama 的解法](https://leetcode.cn/problems/evaluate-division/solutions/1536352/yukiyama-by-yukiyama-geql/)
 
