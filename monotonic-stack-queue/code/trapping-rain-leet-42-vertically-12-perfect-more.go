fba4d0eafc44a97110e37781c56847165dde40a7
Lyu Huafeng <lvhuafeng@gmail.com>
2024-01-06 21:43:12 +0800

    monotonic stack, trapping rain, best approach updated

--- monotonic-stack-queue/code/trapping-rain-leet-42-vertically-12-perfect-more.go
+++ monotonic-stack-queue/code/trapping-rain-leet-42-vertically-12-perfect-more.go
@@ -18,3 +18,21 @@ func trap(height []int) int {
     }
     return sum
 }
+
+// 最佳方法，比上法更简单点
+func trap_best(height []int) int {
+    n := len(height)
+    hl, hr := make([]int, n), make([]int, n)
+    hl[0], hr[n - 1] = height[0], height[n - 1]
+    for i := 1; i <= n - 1 - 1; i++ {
+        j := n - 1 - i
+        hl[i], hr[j] = max(hl[i - 1], height[i]), max(hr[j + 1], height[j])
+    }
+
+    sum := 0
+    // 去掉头尾，因头尾两柱肯定无法盛水
+    for i := 1; i <= n - 1 - 1; i++ {
+        sum += (min(hl[i], hr[i]) - height[i]) * 1
+    }
+    return sum
+}
